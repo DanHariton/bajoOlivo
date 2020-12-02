36718e3e2740cd5f8f90a5e42e46284d8eb5ea98
DanHariton <danilhariton@gmail.com>
2020-12-02 14:40:14 +0100

    updage ci sh

--- ci.sh
+++ ci.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-cd ${WORKSPACE}
-cp /code/carShowroom/.env.local ${WORKSPACE}/.env.local
+cd /home/ubuntu/code/bajoOlivo
 composer install
 php bin/console doctrine:database:create
 php bin/console doctrine:schema:update --dump-sql --force
 npm i
 npm run prod
-sudo ln -sfn ${WORKSPACE}/public /var/www/html
+sudo ln -sfn /home/ubuntu/code/bajoOlivo/public /var/www/html
 sudo service httpd restart
